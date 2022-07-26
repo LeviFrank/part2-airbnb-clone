09335aa2beeb9c956476e328eb25716b28ca40b8
Levi Frank <LeviOrionFrank@gmail.com>
2022-07-26 10:48:39 -0400

    finished lesson 14 :)

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,38 @@
-import React from 'react';
-import './styles.css';
-import Hero from "./components/Hero.jsx";
-import Card from "./components/Card.jsx";
-import Footer from "./components/Footer.jsx";
-import Contacts from "./components/Contacts.jsx";
-import mrWhiskersonSrc from "./images/mrWhiskerson.jpeg"; 
-import fluffyPantsSrc from "./images/fluffyPants.jpeg";
-import samSrc from "./images/sam.jpeg";
-import pumpkinSrc from "./images/pumpkin.jpeg";
+import React from "react";
+import Joke from "./Components/Joke.jsx";
+import Suggestions from "./Components/Suggestions.jsx"
 
-function App () {
-  return (
-    <div className="App">
-      {/* <Hero/>
-      <Card/>
-      <Footer/> */}
-      <Contacts 
-        imageSrc={mrWhiskersonSrc} 
-        name="Mr. Whiskerson"
-        phoneNum="212-555-1847"
-        email="mr.whiskaz@catnap.gov"
-      />
-      <Contacts 
-        imageSrc={fluffyPantsSrc} 
-        name="Fluffy Pants"
-        phoneNum="212-937-7496"
-        email="fuffster@claws.com"
-      />
-      <Contacts
-        imageSrc={samSrc} 
-        name="Sam"
-        phoneNum="212-395-9335"
-        email="sam@thecat.org"
-      />
-      <Contacts
-        imageSrc={pumpkinSrc} 
-        name="Pumpkin"
-        phoneNum="584-727-7648"
-        email="pumpkin@gmail.com"
-      />
-    </div>
-  )
-}
+function App() {
+    return (
+        <div className="app">
+            <Suggestions/>
+            <Joke
+                oneLiner="Just to clarify, when I say I cleaned my room,
+                I mean that I cleared a path from the door to the bed."
+            />
+            <Joke
+                setup="Why are frogs so happy?"
+                punchline="They eat whatever bugs them."
+            />
+            <Joke
+                setup="Want to hear a construction joke?"
+                punchline="Sorry, I'm still working on it."
+                isFunny={false}
+            />
+            <Joke
+                setup="Why should I not fart in an Apple store?"
+                punchline="They don't have windows."
+            />
+            <Joke
+                setup="What is the tallest building in the world?"
+                punchline="The library, because it has so many stories."
+            />
+            <Joke
+                setup="How do you know carrots are good for your eyes?"
+                punchline="You never see a rabbit wearing glasses."
+            />
+        </div>
+    )
+} 
 
 export default App;
---
+++ src/Components/Suggestions.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Suggestions() {
+    return (
+        <div>
+            <p className="suggestionBox">
+                Do you like these Jokes?  
+                <button className="suggestionYes">Yes!</button>
+                <button className="suggestionNo">No!</button>
+                <br></br>
+                <br></br>
+            </p>
+        </div>
+    )
+}
+
+export default Suggestions;
