61fcb0a27b1250402d6eaad29c16bcca0bf8c00f
Levi Frank <LeviOrionFrank@gmail.com>
2022-07-29 15:29:00 -0400

    finished challenge 23

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,8 +2,17 @@ import star from "../images/star.png"
 import React from "react";
 
 function Card(props) {
+    let soldText
+    if (props.openSpots === 0) {
+        soldText = "SOLD OUT"
+    } else if (props.location === "Online") {
+        soldText = "ONLINE"
+    }
+    // SOLD OUT 😥
     return (
         <div className="card">
+            {soldText &&
+            <div className="soldOutTag">{soldText}</div>}
                <img src={props.coverImg} className="cardImages"/>
             <div className="cardStats">
                 <img src={star} className="star"/>
@@ -12,8 +21,8 @@ function Card(props) {
                 <span className="gray">{props.location}</span>
             </div>
             <p className="cardTitle">{props.title}</p>
-            <p><span className="bold">From ${props.price}</span>
-             / person</p></div>
+            <p><span className="bold">From ${props.price} </span> 
+            / person</p></div>
     )
 }
 
